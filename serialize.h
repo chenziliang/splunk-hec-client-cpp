2f937feedcd2a24f067424e1d87f3c8550d5fa68
Ken Chen <kchen@splunk.com>
2017-12-13 21:14:52 -0800

    refactor rapidjson includes

--- serialize.h
+++ serialize.h
@@ -5,8 +5,8 @@
 #ifndef SPLUNK_HEC_CLIENT_CPP_SERIALIZE_H
 #define SPLUNK_HEC_CLIENT_CPP_SERIALIZE_H
 
-#include "rapidjson/include/rapidjson/writer.h"
-#include "rapidjson/include/rapidjson/stringbuffer.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
 
 #include <string>
 #include <cstddef>
