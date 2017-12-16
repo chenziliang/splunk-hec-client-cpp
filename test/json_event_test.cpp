ff7d28feb983ab3fabcc3fa50e31d40871548779
Ken Chen <kchen@splunk.com>
2017-12-15 22:14:49 -0800

    unit test

--- test/json_event_test.cpp
+++ test/json_event_test.cpp
@@ -18,13 +18,41 @@ TEST_CASE("json_event", "[JsonEvent]") {
     string nihao{"ni hao"};
     auto event = JsonEvent<string>(hello, nihao);
 
+    SECTION("fields") {
+        auto& got = event.fields();
+        REQUIRE(got.size() == 0);
+    }
+
     SECTION("add_fields") {
-        map<string, string> fields{{"ni", "world"}};
+        map<string, string> fields{{"ni", "hao"}};
         event.add_fields(fields);
 
-        auto got = event.fields();
+        auto& got = event.fields();
         REQUIRE(got.size() == 1);
-        REQUIRE(got["ni"] == "world");
+        REQUIRE(got.at("ni") == "hao");
+
+        map<string, string> fields2{{"hello", "world"}};
+        event.add_fields(fields2);
+
+        auto& got2 = event.fields();
+        REQUIRE(got2.size() == 2);
+        REQUIRE(got2.at("hello") == "world");
+    }
+
+    SECTION("set_fields") {
+        map<string, string> fields{{"ni", "hao"}};
+        event.set_fields(fields);
+
+        auto& got = event.fields();
+        REQUIRE(got.size() == 1);
+        REQUIRE(got.at("ni") == "hao");
+
+        map<string, string> fields2{{"hello", "world"}};
+        event.set_fields(fields2);
+
+        auto& got2 = event.fields();
+        REQUIRE(got2.size() == 1);
+        REQUIRE(got2.at("hello") == "world");
     }
 
 }
